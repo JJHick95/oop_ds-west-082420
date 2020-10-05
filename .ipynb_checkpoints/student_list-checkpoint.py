2a5e16e8abdd279c9f73bc91fd3e961b5cbd89f1
Max <jmax.barry@gmail.com>
2020-10-05 09:51:55 -0500

    Fix hex id variable to equal 3

--- .ipynb_checkpoints/student_list-checkpoint.py
+++
@@ -1,7 +0,0 @@
-student_first_names = [
-"Ali","Andrew","Angie","Elena",
-"Hunter","Jeffrey","Jonathan",
-"Josh","Karim","Matthew",
-"Ozair","Paul","Prabhakar",
-"Reuben",
-"Sam","Sindhu","William"]
